f41833e2fbb3a87baba4ec802125f55688f28901
perceptron007 <dprophecyguy@gmail.com>
2018-06-03 21:47:01 +0530

    Implemented functionality to create a connection request and announce request, added utility function to generate a unique Id for new client

--- tracker.js
+++ tracker.js
@@ -3,6 +3,10 @@
 const dgram = require('dgram');
 const Buffer = require('buffer').Buffer;
 const urlParse = require('url').parse;
+const crypto = require('crypto');
+
+const torrentParser = require('./torrent-parser');
+const util = require('./util');
 
 module.exports.getPeers = (torrent, callback) => {
   const socket = dgram.createSocket('udp4');
@@ -16,7 +20,7 @@ module.exports.getPeers = (torrent, callback) => {
       // 2. Recieve and parse connect response
       const connResp = parseConnResp(response);
       // 3. Send announce request.
-      const announceReq = buildAnnounceReq(connResp.connectionId);
+      const announceReq = buildAnnounceReq(connResp.connectionId, torrent);
       udpSend(socket, announceReq, url);
     } else if (respType(response) === 'announce') {
       // 4. Parse announce response.
@@ -35,16 +39,105 @@ function respType(response) {
 
 }
 
-function buildConnReq() {}
+function buildConnReq() {
+  // Empty buffer of 16 bytes
+  const buf = Buffer.alloc(16);
 
-function parseConnResp(response) {
+  //connection id
+  // unsigned 32-bit integer in big-endian format
+  // 0x specifies that the number is an hexadecimal number.
+  // We are going to write these two in split 4 bytes values because node doesn't have 8bytes insertion method
+  buf.writeUInt32BE(0x417, 0); // value 0x417 with offset of 4 bytes
+  buf.writeUInt32BE(0x27101980, 4); // value 0x27101980 with offset of 4 bytes
+
+
   
+
+
+  // action id
+  // we write 4byte with value 0 and set offset at 8
+  buf.writeUInt32BE(0, 8);
+
+  // transaction id
+  // create a random 4 bytes buffer and copy it into buf starting at offset 12
+  crypto.randomBytes(4).copy(buf, 12);
+
+  return buf;
 }
 
-function buildAnnounceReq(connectionId) {
+function parseConnResp(response) {
+  return {
+    action: response.readUInt32BE(0),
+    transactionId: response.readUInt32BE(4),
+    connectionId: response.slice(8)
+  }
+}
 
+/*
+
+This is what we have to pass inside createAnnounceReq
+Offset  Size    Name    Value
+0       64-bit integer  connection_id
+8       32-bit integer  action          1 // announce
+12      32-bit integer  transaction_id
+16      20-byte string  info_hash
+36      20-byte string  peer_id
+56      64-bit integer  downloaded
+64      64-bit integer  left
+72      64-bit integer  uploaded
+80      32-bit integer  event           0 // 0: none; 1: completed; 2: started; 3: stopped
+84      32-bit integer  IP address      0 // default
+88      32-bit integer  key             ? // random
+92      32-bit integer  num_want        -1 // default
+96      16-bit integer  port            ? // should be betwee
+98
+*/
+function buildAnnounceReq(connId, torrent, port = 6881) {
+  const buf = Buffer.allocUnsafe(98); // Allocate 98 bytes
+
+  // connection id
+  connId.copy(buf, 0);
+
+  //action id
+  buf.writeUInt32BE(1, 8);
+
+  //transaction id
+  crypto.randomBytes(4).copy(buf, 12);
+
+  //info hash
+  torrentParser.infoHash(torrent).copy(buf, 16);
+
+  // peerId
+  util.genId().copy(buf, 36);
+
+  //download
+  Buffer.alloc(8).copy(buf, 56);
+
+  //left
+  torrentParser.size(torrent).copy(buf, 64);
+
+  // uploaded
+  Buffer.alloc(8).copy(buf, 72);
+
+  // event
+  buf.writeUInt32BE(0, 80);
+
+  // ip address
+  buf.writeUInt32BE(0, 80);
+
+  // key
+  crypto.randomBytes(4).copy(buf, 88);
+
+  // num want 
+  buf.writeInt32BE(-1, 92);
+
+  // port
+  buf.writeUInt32BE(port, 96);
+
+  return buf;
 }
 
 function parseAnnounceReq(response) {
   
 }
+
---
+++ util.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const crypto = require('crypto');
+
+let id = null;
+
+// Utility function to generate a unique id
+module.exports.genId = () => {
+  if(!id) {
+    id = crypto.randomBytes(20);
+    Buffer.from('-AT0001-').copy(id, 0);
+  }
+  return id;
+}
